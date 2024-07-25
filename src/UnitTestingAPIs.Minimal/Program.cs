4ba87fb49b43e01746fafe5a20d9fcdd7f5537aa
joey fawad <mojofawad@gmail.com>
2024-07-24 20:13:51 -0400

    remove unused code

--- src/UnitTestingAPIs.Minimal/Program.cs
+++ src/UnitTestingAPIs.Minimal/Program.cs
@@ -18,21 +18,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast = Enumerable.Range(1, 5).Select(index =>
-//                 new WeatherForecast
-//                 (
-//                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//                     Random.Shared.Next(-20, 55),
-//                     summaries[Random.Shared.Next(summaries.Length)]
-//                 ))
-//             .ToArray();
-//         return forecast;
-//     })
-//     .WithName("GetWeatherForecast")
-//     .WithOpenApi();
-
 app.MapGet("/weatherforecast", WeatherForecastsMinimal.GetWeatherForecasts)
     .WithName("GetWeatherForecast")
     .WithOpenApi();
