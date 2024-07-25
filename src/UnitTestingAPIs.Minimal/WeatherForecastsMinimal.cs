78848154a83f3056f78809676a6278fd2485aa93
joey fawad <mojofawad@gmail.com>
2024-07-24 20:12:29 -0400

    add projects

---
+++ src/UnitTestingAPIs.Minimal/WeatherForecastsMinimal.cs
@@ -0,0 +1,26 @@
+namespace UnitTestingAPIs.Minimal;
+
+public static class WeatherForecastsMinimal
+{
+    public static WeatherForecast[] GetWeatherForecasts()
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        return Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+    }
+}
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
